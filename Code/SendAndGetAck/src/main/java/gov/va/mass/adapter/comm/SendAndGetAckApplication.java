4e37dd84bee335988e74814dbf9c163526bd1f22
avo <avolkano-epic@users.noreply.github.com>
2017-11-03 15:11:07 -0500

    Sender updates
    +DB queue
    +more config

--- Code/SendAndGetAck/src/main/java/gov/va/mass/adapter/comm/SendAndGetAckApplication.java
+++ Code/SendAndGetAck/src/main/java/gov/va/mass/adapter/comm/SendAndGetAckApplication.java
@@ -1,13 +1,9 @@
 package gov.va.mass.adapter.comm;
 
-import javax.jms.Queue;
-
-import org.apache.activemq.command.ActiveMQQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
@@ -19,10 +15,5 @@ public class SendAndGetAckApplication {
 	public static void main(String[] args) {
 		logger.info("SendandgetackApplication running in environment: " + System.getenv("ENV"));
 		SpringApplication.run(SendAndGetAckApplication.class, args);
-	}
-
-	@Bean
-	public Queue dbqueue() {
-		return new ActiveMQQueue("writetodb.queue");
 	}
 }
--- Code/SendAndGetAck/src/main/java/gov/va/mass/adapter/comm/SendAndGetAckService.java
+++ Code/SendAndGetAck/src/main/java/gov/va/mass/adapter/comm/SendAndGetAckService.java
@@ -1,9 +1,9 @@
 package gov.va.mass.adapter.comm;
 
 import java.io.IOException;
-
-import javax.jms.JMSException;
-import javax.jms.Queue;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,7 +30,6 @@ import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.util.Terser;
 
-
 /*
  * Pick up messages from readytosendtoepic queue and send to epic via hl7 over hapi
  * Receive acknowledgement and write to Ack queue
@@ -40,150 +39,135 @@ import ca.uhn.hl7v2.util.Terser;
  * TODO : When exception happens should do a retry only for ioexception, then send to error queue
  * TODO save response to database or queue to database
  */
-
 @Component
 @PropertySource("classpath:application.properties")
 public class SendAndGetAckService {
-	
-	@Value("${keystore.location}")
-	private String KEYSTORE_LOCATION ;
-			
-	@Value("${keystore.password}")
-	private String KEYSTORE_PASSWORD ;
-	
-	@Value("${keystore.type}")
-	private String KEYSTORE_TYPE ;
-	
-	@Value("${tls.destination.host}")
-	private String TLS_DESTINATION_HOST ;
-	
-	@Value("${tls.destination.port}")
-	private int TLS_DESTINATION_PORT ; 
-	
-	@Value("${tls.destination.uri}")
-	private String TLS_DESTINATION_URI;
-	
+	@Value("${tls.keystore.location}")
+	private String KEYSTORE_LOCATION;
+
+	@Value("${tls.keystore.password}")
+	private String KEYSTORE_PASSWORD;
+
+	@Value("${tls.keystore.type}")
+	private String KEYSTORE_TYPE;
+
+	@Value("${tls.enabled}")
+	private boolean TLS_ENABLED = true;
+
+	@Value("${destination.url}")
+	private String DESTINATION_URL;
+
 	@Value("${app.name}")
-	private  String appname;
-	
+	private String appname;
+
 	@Value("${maxattempts.epicsend}")
-	private int MAX_SEND_ATTEMPTS ; 
-	
+	private int MAX_SEND_ATTEMPTS;
+
 	@Value("${sendattempt.interval}")
-	private int SEND_ATTEMPT_INTERVAL ;
-	
-	private int sendattemptcounter = 0 ;
-	
-	@Autowired
-	private Queue dbqueue;
-	
+	private int SEND_ATTEMPT_INTERVAL;
+
+	private int sendattemptcounter = 0;
+
 	@Autowired
 	private JmsMessagingTemplate jmsMsgTemplate;
-	
+
+	@Value("${jms.databaseQ}")
+	private String databaseQueue;
+
+	@Value("${interface.id}")
+	String interfaceId;
+
+	@Value("${index.fieldList}")
+	String fieldList;
+
 	private static final Logger logger = LoggerFactory.getLogger(SendAndGetAckService.class);
-	
-	@JmsListener(destination = "readytosendtoepic.queue")
-	public void sendMessagesToEpicFromReadyQueue ( String msgtxt) {
-		
-		logger.info("Received from Q: "+ msgtxt);
-		
+
+	@JmsListener(destination = "${jms.inputQ}")
+	public void sendMessagesToEpicFromReadyQueue(String msgtxt) throws MalformedURLException {
+		logger.info("Received from Q: " + msgtxt);
+		sendMessageToWriteToDBQueue(msgtxt);
 		putMsgIdAndSenderOnMDC(msgtxt);
-		
-	    sendToEpicOverHapi ( msgtxt);	
-	    
-	    MDC.clear();
+		sendToEpicOverHapi(msgtxt);
+		MDC.clear();
 		logger.debug("MDC cleared");
 	}
 
-	private void sendToEpicOverHapi(String msgtxt) {
-		
-		CustomCertificateTlsSocketFactory customtlsSF = new CustomCertificateTlsSocketFactory( KEYSTORE_TYPE, KEYSTORE_LOCATION , KEYSTORE_PASSWORD  );
-
-		HohRawClientSimple client = new HohRawClientSimple (TLS_DESTINATION_HOST, TLS_DESTINATION_PORT, TLS_DESTINATION_URI);// parser);
-		client.setSocketFactory( customtlsSF );
+	private void sendToEpicOverHapi(String msgtxt) throws MalformedURLException {
+		HohRawClientSimple client = new HohRawClientSimple(new URL(DESTINATION_URL));
+		CustomCertificateTlsSocketFactory customtlsSF;
+		if (TLS_ENABLED) {
+			customtlsSF = new CustomCertificateTlsSocketFactory(KEYSTORE_TYPE, KEYSTORE_LOCATION, KEYSTORE_PASSWORD);
+			client.setSocketFactory(customtlsSF);
+		}
 		ISendable rawsendable = new RawSendable(msgtxt);
-		
 		EncodingStyle es = rawsendable.getEncodingStyle();
-		logger.debug( " rawsendable " +
-				" encoding style " + es.toString()+ 
-				" content type " + es.getContentType() ); 
-		sendattemptcounter = 0 ;
+		logger.debug(" rawsendable " + " encoding style " + es.toString() + " content type " + es.getContentType());
+		sendattemptcounter = 0;
 		do {
 			try {
 				++sendattemptcounter;
-				logger.info(" \n\nSending to EPIC attempt# "+sendattemptcounter+"\n " +
-						rawsendable.getMessage().toString());
-				IReceivable<String> receivable = client.sendAndReceive(rawsendable );
+				logger
+						.info(" \n\nSending to EPIC attempt# " + sendattemptcounter + "\n " + rawsendable.getMessage().toString());
+				IReceivable<String> receivable = client.sendAndReceive(rawsendable);
 				String respstring = receivable.getMessage();
-				logger.info("\nEpic response :\n" + respstring + "\n"  );
-				sendMessageToWriteToDBQueue (respstring );
+				logger.info("\nEpic response :\n" + respstring + "\n");
+				sendMessageToWriteToDBQueue(respstring);
 				break;
 			} catch (DecodeException | IOException | EncodeException e) {
-                if (sendattemptcounter == MAX_SEND_ATTEMPTS ) {
-                	logger.info("MAX attempts to send to EPIC exceeded.");
-                	writeMessageToErrorQueue ();
-                	break;
-                }
-                else
-                	logger.debug("Wait for a certain period, before reattempting to send. Or push this on a hold queue");
-               
-             try {
-				Thread.sleep (SEND_ATTEMPT_INTERVAL);
-			} catch (InterruptedException e1) {
-				// TODO: what would cause this?
-				// what is the best response to this situation?
+				if (sendattemptcounter == MAX_SEND_ATTEMPTS) {
+					logger.info("MAX attempts to send to EPIC exceeded.");
+					writeMessageToErrorQueue();
+					break;
+				} else
+					logger.debug("Wait for a certain period, before reattempting to send. Or push this on a hold queue");
+				try {
+					Thread.sleep(SEND_ATTEMPT_INTERVAL);
+				} catch (InterruptedException e1) {
+					// TODO: what would cause this?
+					// what is the best response to this situation?
+				}
 			}
-                
-			} 
-		} while ( sendattemptcounter < MAX_SEND_ATTEMPTS );
+		} while (sendattemptcounter < MAX_SEND_ATTEMPTS);
 	}
-	
-	
-	private void sendMessageToWriteToDBQueue(String msg)  {
-		try {
-			jmsMsgTemplate.convertAndSend(dbqueue, msg); 
-			logger.info( "Forwarded to queue = " + dbqueue.getQueueName()  );
-		} catch ( JMSException je ) {
-			logger.error( "Problem occured when accessing the queue name. Name may be wrong or queue may not exist." + je.getMessage()  );
-		} 
 
-
+	private void sendMessageToWriteToDBQueue(String msg) {
+		// Create the Hashmap for MapMessage JMS queue.
+		HashMap<String, Object> mmsg = new HashMap<String, Object>();
+		// Build the MapMessage
+		mmsg.put("messageContent", msg);
+		mmsg.put("fieldList", fieldList);
+		mmsg.put("interfaceId", interfaceId);
+		jmsMsgTemplate.convertAndSend(databaseQueue, mmsg); // send to the database
+		logger.info("Forwarded to queue = " + databaseQueue);
 	}
-	
-	
+
 	private void writeMessageToErrorQueue() {
 		logger.info("At this point will be marking this message as unable to send to EPIC");
 	}
 
-
 	// TODO : close HapiContext at the correct point
 	private void putMsgIdAndSenderOnMDC(String msg) {
-
 		HapiContext context = new DefaultHapiContext();
 		Parser p = context.getGenericParser();
 		Message hapiMsg = null;
-		String sendingApplication = null ;
+		String sendingApplication = null;
 		String msgid = null;
-
-		
 		try {
 			hapiMsg = p.parse(msg);
 		} catch (HL7Exception e) {
 			logger.error("Unable to parse message.");
 			e.printStackTrace();
-		}	;
+		}
+		;
 		Terser terser = new Terser(hapiMsg);
-
 		try {
 			sendingApplication = terser.get("/.MSH-3-1");
 			msgid = terser.get("/MSH-10");
-			
 		} catch (HL7Exception e) {
 			logger.error("Unable to parse message to get the Msg Id and Sender.");
 			e.printStackTrace();
 		}
-
-		MDC.put("MsgId", msgid); 
+		MDC.put("MsgId", msgid);
 		MDC.put("Sender", sendingApplication);
 	}
 }
