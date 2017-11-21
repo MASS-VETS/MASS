de20b02264d9c782a8e8dca396aeffb38ec94a2e
lalitag <31447226+lalitag@users.noreply.github.com>
2017-11-21 15:59:51 -0500

    Mock ensemble service to test audiocare get and post

---
+++ Code/mockaudioensemble/src/test/java/gov/va/mass/adapter/mock/MockaudioensembleApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.va.mass.adapter.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockaudioensembleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
