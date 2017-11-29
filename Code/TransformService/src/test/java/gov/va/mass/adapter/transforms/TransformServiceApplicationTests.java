44f020565352cb879638171a13dacf2f3a3ab7f5
dave-epic <33354303+dave-epic@users.noreply.github.com>
2017-11-28 18:17:43 -0600

    uncommitting accidental commit

--- Code/TransformService/src/test/java/gov/va/mass/adapter/transforms/TransformServiceApplicationTests.java
+++ Code/TransformService/src/test/java/gov/va/mass/adapter/transforms/TransformServiceApplicationTests.java
@@ -122,30 +122,3 @@ public class TransformServiceApplicationTests {
 		LOG.debug(output);
 		assertEquals(expected, output);
 	}
-		
-	@Test
-	// Patient Update Messages (ADT^A28)
-	public void testDLUMessage5() throws HL7Exception, MicroserviceException {
-		String pipeMsg = "MSH|^~\\&|DG-REG-OUT|500^HL7.VEHU.DOMAIN.GOV:5026^DNS|MASS|^dhcp.epic.com:5106^DNS|20171010163409-0500||ADT^A08^|500 9406|T^|2.4|||AL|NE\r"
-				+ "PID|1||3^^^USVHA^IEN~10108^^^USVHA^NI||DXL^PA^^^^||19350409|M|P8^^^^^~DXL^TEST^^^^||123 Conf Street^Addr Line 2^Knoxville^TN^37865^^C^||(222)555-8235^^P^DXLTEST@email.com|(222)555-7720|||||666000008|||||||||\r"
-				+ "PV1|1|||||||||||||||||SC VETERAN\r"
-				+ "ZCT|1|DXL^EMERGENCY|Brother|123 Emergency St^Apt 111^Madison^WI^^^^|(865)776-0002\r"
-				+ "ZEL|1|SC LESS THAN 50%||||||||VERIFIED|||||||||||||||||||||||||||YES|20090917\r"
-				+ "ZEL|2|ALLIED VETERAN|||||||||||||||||||||||||||||||||||\r"
-				+ "ZEL|3|WORLD WAR I|||||||||||||||||||||||||||||||||||\r" + "ZEN|1|||UNVERIFIED|||||GROUP 1|||\r"
-				+ "ZSP|1|Y|50|||||||||CLASS II\r";
-		
-		String expected = "MSH|^~\\&|DG-REG-OUT|500^HL7.VEHU.DOMAIN.GOV:5026^DNS|MASS|^dhcp.epic.com:5106^DNS|20171010163409-0500||ADT^A31|500 9406|T|2.4|||AL|NE\r"
-				+ "PID|1||3^^^USVHA^IEN~10108^^^USVHA^NI||DXL^PA||19350409|M|P8~DXL^TEST||123 Conf Street^Addr Line 2^Knoxville^TN^37865^^C||(222)555-8235^^P^DXLTEST@email.com|(222)555-7720|||||666000008\r"
-				+ "NK1|1||123 Emergency St Apt 111 Madison WI\r"
-				+ "PV1|1|||||||||||||||||SC VETERAN\r"
-				+ "ZFY|SERVICE CONNECTED|ACTIVE|SERVICE CONNECTED|50% Service Connected\r"
-				+ "ZEL|SC LESS THAN 50%~ALLIED VETERAN~WORLD WAR I|CLASS II|GROUP 1|UNVERIFIED|YES|20090917\r";
-		
-		svc.xsltName = "ADT_A08";
-		JmsResponse<String> response = svc.transformPipeMessage(pipeMsg);
-		String output = response.getResponse();
-		LOG.debug(output);
-		assertEquals(expected, output);
-	}
-}
