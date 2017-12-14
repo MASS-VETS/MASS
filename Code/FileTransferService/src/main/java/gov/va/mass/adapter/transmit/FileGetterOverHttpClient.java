a7fcc98e77fd192ef1bb131c485fbba56b95ff64
avo <avolkano-epic@users.noreply.github.com>
2017-12-14 09:57:20 -0600

    update request mapping
    update request mapping so two classes in the same project can inherit from MicroserviceCore

--- Code/FileTransferService/src/main/java/gov/va/mass/adapter/transmit/FileGetterOverHttpClient.java
+++ Code/FileTransferService/src/main/java/gov/va/mass/adapter/transmit/FileGetterOverHttpClient.java
@@ -1,19 +1,15 @@
 package gov.va.mass.adapter.transmit;
 
 import java.io.File;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 import java.util.HashMap;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-
 import org.apache.http.client.methods.HttpGet;
-
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,25 +18,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-
-import org.springframework.util.FileCopyUtils;
-
 import org.springframework.jms.core.JmsMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import gov.va.mass.adapter.core.JmsMicroserviceBase;
+import org.springframework.web.bind.annotation.RestController;
+import gov.va.mass.adapter.core.MicroserviceBase;
 import gov.va.mass.adapter.core.MicroserviceException;
 
 @RestController
+@RequestMapping("/adapter/filetransferservice/getter")
 @PropertySource("classpath:application.properties")
-public class FileGetterOverHttpClient extends JmsMicroserviceBase {
-
+public class FileGetterOverHttpClient extends MicroserviceBase {
+	
 	@Value("${destination.url.get}")
 	private String DESTINATION_URL_GET;
-
+	
 	@Autowired
 	private JmsMessagingTemplate jmsMsgTemplate;
 	
@@ -51,41 +45,41 @@ public class FileGetterOverHttpClient extends JmsMicroserviceBase {
 	private String interfaceId;
 	
 	private TLSHttpClientProvider tlsHttpClientProvider;
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(FileGetterOverHttpClient.class);
-
+	
 	private File saveStreamFile(InputStream is) throws IOException {
 		
 		File tempFile = null;
 		
 		tempFile = stream2file(is);
 		byte[] filebytes = FileCopyUtils.copyToByteArray(tempFile);
-
+		
 		logger.debug("File saved of size " + filebytes.length);
-
+		
 		// Send to the database
-		if(databaseQueue != null && !databaseQueue.isEmpty()) {
-			//Provided that this executed, log to the database that this happened.
+		if (databaseQueue != null && !databaseQueue.isEmpty()) {
+			// Provided that this executed, log to the database that this happened.
 			// Get current date time for later.
 			String dateTime = String.format("%1$tF %1$tT", new Date());
 			
 			// Create the HashMap for MapMessage JMS queue.
 			HashMap<String, Object> mmsg = new HashMap<String, Object>();
-
+			
 			// Build the MapMessage
 			mmsg.put("messageContent", new String(filebytes));
-			mmsg.put("fieldList", ""); //There are not fields to be stored for this interface.
+			mmsg.put("fieldList", ""); // There are not fields to be stored for this interface.
 			mmsg.put("interfaceId", interfaceId);
 			mmsg.put("dateTime", dateTime);
-
+			
 			jmsMsgTemplate.convertAndSend(databaseQueue, mmsg);
 			logger.info("Forwarded to queue = " + databaseQueue);
 		}
-
+		
 		return tempFile;
 	}
 	
-	public static File stream2file (InputStream in) throws IOException {
+	public static File stream2file(InputStream in) throws IOException {
 		final File tempFile = File.createTempFile("stream2file", ".tmp");
 		tempFile.deleteOnExit();
 		try (FileOutputStream out = new FileOutputStream(tempFile)) {
@@ -94,37 +88,37 @@ public class FileGetterOverHttpClient extends JmsMicroserviceBase {
 		return tempFile;
 	}
 	
-
-	@RequestMapping(value = "/adapter/audiocare/responses", method = RequestMethod.GET, produces = "text/csv")
-	public @ResponseBody HttpEntity<byte[]> getAudiocareResponseToLastAppointmentFile() throws IOException, MicroserviceException {
-
+	@RequestMapping(value = "/get", method = RequestMethod.GET, produces = "text/csv")
+	public @ResponseBody HttpEntity<byte[]> getAudiocareResponseToLastAppointmentFile()
+			throws IOException, MicroserviceException {
+		
 		logger.debug("Connecting to Ensemble to obtain Audiocare responses for the last file");
 		this.state.serviceCalled();
-
+		
 		CloseableHttpClient httpClient = null;
 		HttpResponse response = null;
 		HttpEntity<byte[]> httpEntity = null;
 		
 		HttpGet httpGet = null;
-
+		
 		try {
-			httpClient =  tlsHttpClientProvider.getTLSHttpClient(); //builder.build();
+			httpClient = tlsHttpClientProvider.getTLSHttpClient(); // builder.build();
 			httpGet = new HttpGet(DESTINATION_URL_GET);
-
+			
 			httpGet.setHeader(HttpHeaders.ACCEPT, "text/csv");
 			response = httpClient.execute(httpGet);
 			org.apache.http.HttpEntity entity = response.getEntity();
-
+			
 			int responseCode = response.getStatusLine().getStatusCode();
-
+			
 			System.out.println("Request Url: " + httpGet.getURI());
 			System.out.println("Response Code: " + responseCode);
-															
+			
 			InputStream is = entity.getContent();
-			File file = saveStreamFile(is); 
+			File file = saveStreamFile(is);
 			is.close();
 			byte[] filebytes = FileCopyUtils.copyToByteArray(file);
-						
+			
 			HttpHeaders headers = new HttpHeaders();
 			headers.set("Content-Type", "application/octet_stream");
 			headers.set("Content-Disposition", "attachment; filename=" + file.getName());
@@ -134,32 +128,28 @@ public class FileGetterOverHttpClient extends JmsMicroserviceBase {
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 			this.state.serviceFailed();
-			this.enterErrorState(e.getMessage());	//throws MicroserviceException
 		} catch (UnsupportedOperationException e) {
 			e.printStackTrace();
 			this.state.serviceFailed();
-			this.enterErrorState(e.getMessage());	//throws MicroserviceException
 		} catch (IOException e) {
 			e.printStackTrace();
 			this.state.serviceFailed();
-			this.enterErrorState(e.getMessage());	//throws MicroserviceException
 		} finally {
 			if (httpGet != null) {
 				httpGet.releaseConnection();
 			}
 		}
-
+		
 		return httpEntity;
 	}
-
-
+	
 	public void setTLSHttpClientProvider(TLSHttpClientProvider tlsHttpClientProvider) {
 		this.tlsHttpClientProvider = tlsHttpClientProvider;
 	}
-
+	
 	@Override
 	protected String serviceName() {
-		return "FileTransferService";
+		return "FileGetterService";
 	}
 	
 }
--- Code/FileTransferService/src/main/java/gov/va/mass/adapter/transmit/FileSenderOverHttpClient.java
+++ Code/FileTransferService/src/main/java/gov/va/mass/adapter/transmit/FileSenderOverHttpClient.java
@@ -8,64 +8,55 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.util.Date;
 import java.util.HashMap;
-
 import org.apache.commons.io.IOUtils;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
-
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.PropertySource;
-
-
 import org.springframework.http.HttpHeaders;
-
 import org.springframework.http.HttpStatus;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsMessagingTemplate;
-
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.multipart.MultipartFile;
+import gov.va.mass.adapter.core.MicroserviceBase;
 
 // TODO : Make hostname verification an environment specific parameter
 @RestController
+@RequestMapping("/adapter/filetransferservice/sender")
 @PropertySource("classpath:application.properties")
-public class FileSenderOverHttpClient {
-
+public class FileSenderOverHttpClient extends MicroserviceBase {
+	
 	// Keystore properties here are used when connecting to ensemble
-
+	
 	@Value("${keystore.enabled}")
 	private boolean TLS_ENABLED = false;
-
+	
 	@Value("${keystore.location}")
 	private String KEYSTORE_LOCATION;// = "C:/work/1twowayssl/adapterkeys/adapterks.jks"
-
+	
 	@Value("${keystore.password}")
 	private String KEYSTORE_PASSWORD;
-
+	
 	@Value("${keystore.type}")
 	private String KEYSTORE_TYPE;
-
+	
 	@Value("${destination.url.post}")
 	private String DESTINATION_URL_POST;
-
+	
 	@Value("${interface.id}")
 	private String interfaceId;
 	
@@ -76,37 +67,37 @@ public class FileSenderOverHttpClient {
 	private String databaseQueue;
 	
 	private static final Logger logger = LoggerFactory.getLogger(FileSenderOverHttpClient.class);
-
+	
 	private TLSHttpClientProvider setTLSHttpClientProvider;
-
-	@PostMapping("/adapter/audiocare/epicappointments") // , consumes="text/csv")
+	
+	@PostMapping("/send") // , consumes="text/csv")
 	public ResponseEntity<String> postAppointmentsFileToEnsembleHttpClientBased(
 			@RequestParam("file") MultipartFile uploadfile) {
-
+		
 		logger.debug("Single file upload!");
-
+		
 		if (uploadfile.isEmpty()) {
 			return new ResponseEntity<String>("Please select a file.", HttpStatus.OK);
 		}
-
+		
 		try {
 			File savedfile = saveUploadedFiles(uploadfile);
 			prepareAndPost(savedfile);
 		} catch (IOException e) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-
+		
 		return new ResponseEntity<String>("Successfully uploaded file to Adapter and from Adapter to Ensemble- "
 				+ uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
-
+		
 	}
-
+	
 	private File saveUploadedFiles(MultipartFile file) throws IOException {
 		logger.debug("In saveUploadedFiles " + file.getOriginalFilename());
-
+		
 		byte[] bytes = file.getBytes();
 		
-		//Provided that this executed log to the database that this happened.
+		// Provided that this executed log to the database that this happened.
 		// Get current date time for later.
 		String dateTime = String.format("%1$tF %1$tT", new Date());
 		
@@ -115,12 +106,12 @@ public class FileSenderOverHttpClient {
 		
 		// Build the MapMessage
 		mmsg.put("messageContent", new String(bytes));
-		mmsg.put("fieldList", ""); //There are not fields to be stored for this interface.
+		mmsg.put("fieldList", ""); // There are not fields to be stored for this interface.
 		mmsg.put("interfaceId", interfaceId);
 		mmsg.put("dateTime", dateTime);
 		
 		// Send to the database
-		if(databaseQueue != null && !databaseQueue.isEmpty()) {
+		if (databaseQueue != null && !databaseQueue.isEmpty()) {
 			jmsMsgTemplate.convertAndSend(databaseQueue, mmsg);
 			logger.info("Forwarded to queue = " + databaseQueue);
 		}
@@ -129,8 +120,8 @@ public class FileSenderOverHttpClient {
 		logger.debug("length of saved file " + tempFile.length());
 		return tempFile;
 	}
-		
-	public static File stream2file (InputStream in) throws IOException {
+	
+	public static File stream2file(InputStream in) throws IOException {
 		final File tempFile = File.createTempFile("stream2file", ".tmp");
 		tempFile.deleteOnExit();
 		try (FileOutputStream out = new FileOutputStream(tempFile)) {
@@ -138,18 +129,17 @@ public class FileSenderOverHttpClient {
 		}
 		return tempFile;
 	}
-
 	
 	private void finalPostFile(CloseableHttpClient httpClient, File savedfile) {
-
+		
 		HttpPost httpPost = new HttpPost(DESTINATION_URL_POST);
 		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-
+		
 		builder.addBinaryBody("file", savedfile, ContentType.create("text/csv"), savedfile.getName());
 		builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE); // change mode
-
+		
 		HttpEntity entity = builder.build();
-
+		
 		httpPost.setEntity(entity);
 		try {
 			logger.debug("Posting");
@@ -163,14 +153,18 @@ public class FileSenderOverHttpClient {
 		}
 	}
 	
-
 	private void prepareAndPost(File savedfile) {
 		CloseableHttpClient httpClient = setTLSHttpClientProvider.getTLSHttpClient();
 		finalPostFile(httpClient, savedfile);
 	}
-
+	
 	public void setTLSHttpClientProvider(TLSHttpClientProvider tlsHttpClientProvider) {
 		this.setTLSHttpClientProvider = tlsHttpClientProvider;
 	}
-
+	
+	@Override
+	protected String serviceName() {
+		return "FileGetterService";
+	}
+	
 }
